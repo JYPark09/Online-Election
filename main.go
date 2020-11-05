4c294e81990797811266edf1b463240683a353cd
Junyeong Park <jyp10987@gmail.com>
2020-11-05 11:36:50 +0900

    feat(log): implement log system

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	logFile, err := os.OpenFile(time.Now().Format("./logs/2006_01_02_15_04_05")+".log", os.O_WRONLY|os.O_CREATE, 0775)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	mw := io.MultiWriter(os.Stderr, logFile)
+	log.SetOutput(mw)
 }

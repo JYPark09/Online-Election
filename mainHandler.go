acae5a2b2008a2b280fdffc982a07f039a12f405
Junyeong Park <jyp10987@gmail.com>
2020-11-07 16:09:30 +0900

    feat(view): make election list view

--- mainHandler.go
+++ mainHandler.go
@@ -8,5 +8,21 @@ import (
 func mainHandler(writer http.ResponseWriter, request *http.Request) {
 	t, _ := template.ParseFiles("template/index.html")
 
-	t.Execute(writer, nil)
+	var data struct {
+		During []Election
+		Ready  []Election
+		Done   []Election
+	}
+
+	for _, election := range elections {
+		if election.Status == DURING {
+			data.During = append(data.During, election)
+		} else if election.Status == DONE {
+			data.Done = append(data.Done, election)
+		} else if election.Status == READY {
+			data.Ready = append(data.Ready, election)
+		}
+	}
+
+	t.Execute(writer, &data)
 }
